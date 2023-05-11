a643c982aae97f8b28ef3a2f25812ed941a9c8ca
Wakadaisho <paulwahome9@gmail.com>
2023-05-11 14:55:09 +0300

    Use a loop instead of '.join'

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,8 +2,8 @@
 
 import hidden_4
 
-
 if __name__ == "__main__":
     symbols = [s for s in dir(hidden_4) if not s.startswith('__')]
     symbols.sort()
-    print('\n'.join(symbols))
+    for s in symbols:
+        print(s)
